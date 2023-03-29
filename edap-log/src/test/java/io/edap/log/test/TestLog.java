0ea0f57ecd9384a9139474ffcb1ed134f90b6df6
luysh <luysh@yonyou.com>
2023-03-29 19:44:22 +0800

    完善日志打印的测试用例

--- edap-log/src/test/java/io/edap/log/test/TestLog.java
+++ edap-log/src/test/java/io/edap/log/test/TestLog.java
@@ -10,9 +10,16 @@ import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.ServiceLoader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TestLog {
 
@@ -26,7 +33,24 @@ public class TestLog {
         }
 
         LOG.info("name: {},height: {}", l -> l.arg("edap").arg(90.0));
+
+        File f = new File("./edap.log");
+        String log = readFile(f);
+        assertNotNull(log);
+        f.delete();
+        assertEquals(log.substring(23), " INFO  [main] [io.edap.log.test.TestLog] [] []  - name: edap,height: 90.0 \n");
+
         System.out.println(EdapLogContext.instance());
+    }
+
+    private static String readFile(File f) throws IOException {
+        FileInputStream fis = new FileInputStream(f);
+        byte[] data = new byte[1024];
+        int len = fis.read(data);
+        if (len > 0) {
+            return new String(data, 0, len, StandardCharsets.UTF_8);
+        }
+        return null;
     }
 
 }
