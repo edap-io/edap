d261e777688ab189b757eb44ee42a9df05f7d73b
luysh <luysh@yonyou.com>
2023-03-31 13:56:38 +0800

    添加演示用例

--- edap-json/src/test/java/io/edap/json/test/model/Test2.java
+++ edap-json/src/test/java/io/edap/json/test/model/Test2.java
@@ -7,32 +7,20 @@ import io.edap.json.writer.ByteArrayJsonWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class Test {
+public class Test2 {
     public long id;
     public String name;
     public int age;
     public float aFloat;
     public BigDecimal num;
 
-    public Test2 test2;
-
     public static void main(String[] args) throws IOException {
-        Test test1 = new Test();
+        Test2 test1 = new Test2();
         test1.id = 1L;
         test1.name = "默默";
         test1.age = 26;
         test1.aFloat = 1F;
         test1.num = new BigDecimal("123.2");
-
-        Test2 test2 = new Test2();
-        test2.id = 2L;
-        test2.name = "默默2";
-        test2.age = 226;
-        test2.aFloat = 21F;
-        test2.num = new BigDecimal("2123.2");
-
-        test1.test2 = test2;
-
 
         ByteArrayJsonWriter writer = new ByteArrayJsonWriter(new ByteArrayBufOut());
         Eson.serialize(test1,writer);
