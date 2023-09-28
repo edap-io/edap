41d7861543fd2082662d3dd3cb46c1aed8a5c31f
luysh <luysh@yonyou.com>
2023-09-28 17:09:33 +0800

    将相关模块整理到模块的parent的模块中简化根目录 增加http相关的模块

---
+++ edap-data-parent/edap-data-jdbc-dao/src/test/java/T.java
@@ -0,0 +1,12 @@
+import io.edap.util.CollectionUtils;
+import io.edap.util.Constants;
+
+import java.util.List;
+
+public class T {
+
+    public static void main(String[] args) {
+        List empty = Constants.EMPTY_LIST;
+        empty.add("1");
+    }
+}
